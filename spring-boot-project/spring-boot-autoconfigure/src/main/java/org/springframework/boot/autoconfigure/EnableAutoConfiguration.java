76c56d90787cf07c925996f4ec5d7d516aead238
liujinsuozn8 <43396666+liujinsuozn8@users.noreply.github.com>
2022-03-02 17:33:22 +0800

    update

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/EnableAutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/EnableAutoConfiguration.java
@@ -80,6 +80,7 @@ import org.springframework.core.io.support.SpringFactoriesLoader;
 @Documented
 @Inherited
 @AutoConfigurationPackage
+// 用来导入 starter
 @Import(AutoConfigurationImportSelector.class)
 public @interface EnableAutoConfiguration {
 
